9ce463b4a32da8f0b9b9e31f61b5ada9737dacd8
tunglxx226@gmail.com <tunglxx226@gmail.com@391aae74-6242-2515-78c6-3a42c1ae1add>
2014-06-07 16:55:21 +0000

    tối đen như mực

--- trunk/KnockCode/src/lx/tung/knockcode/KnockOnService.java
+++ trunk/KnockCode/src/lx/tung/knockcode/KnockOnService.java
@@ -1,37 +1,96 @@
 package lx.tung.knockcode;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class KnockOnService extends Service {
 	BroadcastReceiver mReceiver;
+	
+	static SensorManager sensorManager = null;
+	static SensorEventListener aListener = null;
+	static int orientation = -1;
+	static WakeLock wl;
+	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 	    //TODO do something useful
-		boolean screenOn = intent.getBooleanExtra("screen_state", false);
-	    if (!screenOn) {
+		Log.d("KnockOnService", "onStartCommand() start;");
+		boolean screenOff = intent.getBooleanExtra("screen_state", false);
+		boolean knockOn = intent.getBooleanExtra("knockOn", false);
+		
+		Log.d("KnockOnService", "screenOff: " + screenOff);
+	    if (!screenOff) {
+	        if(sensorManager != null && aListener != null){
+	        	Log.d("KnockOnService", "sensorManager.unregister;");
+	        	sensorManager.unregisterListener(aListener);
+	        	sensorManager = null;
+	        	aListener = null;
+	        	Log.d("KnockOnService", "sensorManager == null;");
+	        }
 	        
+	        if(knockOn == true){
+	        	Log.d("KnockOnService", "wake screen release");
+	        	wl.release();
+	        	wl = null;
+	        }
 	    } else {
-	    	WakeLock wl;
-	    	PowerManager pm;
-	    	Intent i = new Intent(this, BlackScreen.class);
-	        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-	        startActivity(i);
-	    	pm = (PowerManager) getSystemService(POWER_SERVICE);
-	    	wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK
-	    			| PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");  wl.acquire();
-	    	wl.acquire();
-	        wl.release();
-	        unregisterReceiver(mReceiver);
-	    	Log.d("SCREEN", "ON");
-	    	this.stopSelf();
+	    	Log.d("KnockOnService", "sensorManager.register;");
+	    	sensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+		    aListener = new SensorEventListener() {
+	            int orientation=-1;
+
+	            @Override
+	            public void onSensorChanged(SensorEvent event) {
+	                if (event.values[1]<6.5 && event.values[1]>-6.5) {
+	                    if (orientation!=1) {
+	                        KnockOnService.orientation = 1;//Landscape
+	                    }
+	                    orientation=1;
+	                } else {
+	                    if (orientation!=0) {
+	                    	KnockOnService.orientation = 0;//Portrait
+	                    	Log.d("KnockOnService", "wake screen");
+	                    	Intent i = new Intent(KnockOnService.this, BlackScreen.class);
+	            	        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	            	        startActivity(i);
+	            	    	PowerManager pm;
+	            	    	pm = (PowerManager) getSystemService(POWER_SERVICE);
+	            	    	wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK
+	            	    			| PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
+	            	    	wl.acquire();
+	            	    	Log.d("KnockOnService", "wake screen acquire");
+	                    	
+	                    }
+	                    orientation=0;
+	                }
+	            }
+
+	            @Override
+	            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	                // TODO Auto-generated method stub
+
+	            }
+	        };
+	        sensorManager.registerListener(aListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+	    	
+//	        unregisterReceiver(mReceiver);
+	    	Log.d("SCREEN", "OFF");
+//	    	this.stopSelf();
 	    }
+	    Log.d("KnockOnService", "onStartCommand() end;");
 	    return Service.START_STICKY;
 	}
 
@@ -49,6 +108,14 @@ public class KnockOnService extends Service {
 	    filter.addAction(Intent.ACTION_SCREEN_OFF);
 	    mReceiver = new ScreenReceiver();
 	    registerReceiver(mReceiver, filter);
+	    
+	    Notification n  = new NotificationCompat.Builder(this)
+        .setContentTitle("Knock On")
+        .setContentText("Knock On activated")
+        .setSmallIcon(R.drawable.ic_launcher).build();
+	    
+	    startForeground (1, n);
+	    
 	    Log.d("KnockOnService", "ONCREATE");
 	}
 } 
